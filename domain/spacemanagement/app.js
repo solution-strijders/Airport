939eb8e8e20362352a5d55b55c06a8e6f6e7b7a9
RDAxRoadkill <wouter97@planet.nl>
2019-05-19 14:10:29 +0200

    Added spacemanagement

---
+++ domain/spacemanagement/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 3002, () => {
+	if (process.env.PORT !== undefined) {
+		console.log(`Server started at "http://localhost:${process.env.PORT}/".`);
+	} else {
+		console.log(`Server started at "http://localhost:3002/".`);
+	}
+});
+
+app.get("/", (req, res) => {
+    return res.send("I am working! | SpaceManagement");
+});
+
+module.exports = app;
