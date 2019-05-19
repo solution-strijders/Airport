cc9175f1a49f71dc6dbcdfd523041456e2f46de9
RDAxRoadkill <wouter97@planet.nl>
2019-05-19 12:05:01 +0200

    Fixed dockerfile by adding host & container file

--- domain/airplanemanagement/app.js
+++ domain/airplanemanagement/app.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	if (process.env.PORT !== undefined) {
 		console.log(`Server started at "http://localhost:${process.env.PORT}/".`);
 	} else {
-		console.log(`Server started at "http://localhost:5000/".`);
+		console.log(`Server started at "http://localhost:3000/".`);
 	}
 });
 
