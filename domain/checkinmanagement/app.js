89d9223ae27b4dae11d8869a3df276ab9a76e6fb
RDAxRoadkill <wouter97@planet.nl>
2019-05-19 16:59:38 +0200

    Added swagger to checkinmanagement

--- domain/checkinmanagement/app.js
+++ domain/checkinmanagement/app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+var router = require("./router");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Swagger document definition
+const options = {
+	definition: {
+		info: {
+			version: 1.0, // Version (Req)
+			title: "Checkin Management", // Title (Req)
+			description: "API to checkin"
+		}
+	},
+	//Path to API docs
+	apis: ["./router.js"]
+}
+
+//Initialize swagger-js doc
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/", router);
 
 app.listen(process.env.PORT || 3003, () => {
 	if (process.env.PORT !== undefined) {
@@ -11,10 +34,6 @@ app.listen(process.env.PORT || 3003, () => {
 	} else {
 		console.log(`Server started at "http://localhost:3003/".`);
 	}
-});
-
-app.get("/", (req, res) => {
-    return res.send("I am working! | CheckinManagement");
 });
 
 module.exports = app;
---
+++ domain/checkinmanagement/router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     description: Introduction screen to application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - none
+ *     responses:
+ *       200:
+ *         description: rootPage
+ */
+router.get("/", (req, res) => {
+    return res.send("Welcome to the Checkin service");
+});
+
+module.exports = router;
