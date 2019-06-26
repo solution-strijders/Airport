36050f701e61e74db92153561a12208c667439fa
Senexis <timoviveen@live.nl>
2019-06-26 13:36:34 +0200

    Initial groundmanagement.

---
+++ domain/groundmanagement/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+var router = require("./router");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Swagger document definition
+const options = {
+	definition: {
+		info: {
+			version: 1.0, // Version (Req)
+			title: "Border Management", // Title (Req)
+			description: "API to protecc"
+		}
+	},
+	//Path to API docs 
+	apis: ["./router.js"]
+}
+
+//Initialize swagger-js doc
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(process.env.PORT || 3002, () => {
+	if (process.env.PORT !== undefined) {
+		console.log(`Server started at "http://localhost:${process.env.PORT}/".`);
+	} else {
+		console.log(`Server started at "http://localhost:3002/".`);
+	}
+});
+
+module.exports = app;
