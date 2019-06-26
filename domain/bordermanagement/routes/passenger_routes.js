bd9d7111345a50bb69e980fc102b7352cfcd1a36
Okke Trommelen <oga.trommelen@student.avans.nl>
2019-06-26 14:30:20 +0200

    do you have drugs in your ass?

---
+++ domain/bordermanagement/routes/passenger_routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const passengerController = require('../controller/passengerController');
+
+/* GET users listing. */
+router.get('/', passengerController.GetPassengers);
+router.post('/', passengerController.CreatePassenger);
+
+module.exports = router;
