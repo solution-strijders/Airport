efd2b8374855fbaa3842c1b32199a14312181866
KRademacher <Kevin_Rademacher@hotmail.com>
2019-06-26 13:56:54 +0200

    update

--- domain/baggagemanagement/models/baggage.js
+++ domain/baggagemanagement/models/baggage.js
@@ -1,13 +1,5 @@
 const mongoose = require('../config/database');
 
-<<<<<<< HEAD
-const baggageSchema = new mongoose.Schema({
-    //TODO: Add plane
-    Weight: {
-        type: Number,
-        required: true,
-        min: 0
-=======
 const flightSchema = require('./flight').schema;
 
 const baggageSchema = new mongoose.Schema({
@@ -18,7 +10,6 @@ const baggageSchema = new mongoose.Schema({
     },
     Flight: {
         type: flightSchema
->>>>>>> 3d682cf859fd0519cdb74bf78c1b55cb8d925f94
     }
 });
 
