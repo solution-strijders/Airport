ac8781855d6320e9d097addea9d3c0d87c444906
Okke Trommelen <oga.trommelen@student.avans.nl>
2019-06-28 11:41:11 +0200

    paul blart

--- domain/securitymanagement_write/rabbot/rabbot.js
+++ domain/securitymanagement_write/rabbot/rabbot.js
@@ -1,9 +1,5 @@
 const rabbot = require("rabbot");
-const bagage = require("../models/bagage");
-const flight = require('../models/flight');
-const passenger = require('../models/passenger');
-const plane = require('../models/plane');
-const user = require('../models/users');
+const Event = require("../models/event");
 require("dotenv").config();
 
 rabbot
@@ -24,6 +20,54 @@ rabbot
         autoDelete: false,
         durable: true,
         subscribe: true
+      },
+      {
+        name: "baggagemanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "bordermanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "checkinmanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "controlmanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "flightmanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "groundmanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "matrixmanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
+      },
+      {
+        name: "spacemanagement_queue",
+        autoDelete: false,
+        durable: true,
+        subscribe: true
       }
     ],
     bindings: [
@@ -31,49 +75,81 @@ rabbot
         exchange: "ex.1",
         target: "financialmanagement_queue",
         keys: ["baggageNoted", "flightNoted"]
+      },
+      {
+        exchange: "ex.1",
+        target: "baggagemanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "bordermanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "checkinmanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "controlmanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "flightmanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "groundmanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "matrixmanagement_queue",
+        keys: [""]
+      },
+      {
+        exchange: "ex.1",
+        target: "spacemanagement_queue",
+        keys: [""]
       }
     ]
   })
   .then(() => {
     console.log("Rabbot succesfully connected.");
     rabbot.startSubscription("financialmanagement_queue");
+    rabbot.startSubscription("baggagemanagement_queue");
+    rabbot.startSubscription("bordermanagement_queue");
+    rabbot.startSubscription("checkinmanagement_queue");
+    rabbot.startSubscription("controlmanagement_queue");
+    rabbot.startSubscription("flightmanagement_queue");
+    rabbot.startSubscription("groundmanagement_queue");
+    rabbot.startSubscription("matrixmanagement_queue");
+    rabbot.startSubscription("spacemanagement_queue");
     console.log("Rabbot subscribed.");
   })
   .catch(error => console.log("Rabbot connect error: " + error));
 
 rabbot.handle("baggageNoted", msg => {
-  new bagage(msg)
-    .save()
-    .then(() => msg.ack())
-    .catch(err => msg.nack());
+  new Event({
+    Type: msg.type,
+    Queue: msg.queue,
+    Body: JSON.stringify(msg.body)
+  })
 });
 
 rabbot.handle("flightNoted", msg => {
-    new flight(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
+  new Event({
+    Type: msg.type,
+    Queue: msg.queue,
+    Body: JSON.stringify(msg.body)
+  })
+});
 
-  rabbot.handle("passengerNoted", msg => {
-    new passenger(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
-  
-  rabbot.handle("planeNoted", msg => {
-    new plane(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
 
-  rabbot.handle("userNoted", msg => {
-    new user(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
+
 
 module.exports = rabbot;
