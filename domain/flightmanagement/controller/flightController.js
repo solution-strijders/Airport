d8150cb14a2ec09e3d16b516ae4faa241f5585ee
Senexis <timoviveen@live.nl>
2019-07-01 15:02:18 +0200

    Fire statusChanged and stowBaggage events.

--- domain/flightmanagement/controller/flightController.js
+++ domain/flightmanagement/controller/flightController.js
@@ -6,49 +6,57 @@ const Airline = require('../models/airline');
 const Plane = require('../models/plane');
 
 module.exports = {
-    GetFlight(req,res,next){
-        Flight.findById({_id: req.params.id})
-        .then((result, err)=> {
-            if(result && !err){
-                res.status(200);
-                res.send(result);
-            }else{
-                res.send(err);
-            }
-        });
+    GetFlight(req, res, next) {
+        Flight.findById({ _id: req.params.id })
+            .then((result, err) => {
+                if (result && !err) {
+                    res.status(200);
+                    res.send(result);
+                } else {
+                    res.send(err);
+                }
+            });
     },
 
-    GetFlights(req, res, next){
+    GetFlights(req, res, next) {
         Flight.find()
-        .then((result, err) => {
-            if(!err){
-                res.status(200);
-                res.send(result);
-            }
-        });
+            .then((result, err) => {
+                if (!err) {
+                    res.status(200);
+                    res.send(result);
+                }
+            });
     },
 
-    ChangeStatus(req,res,next){
-        Flight.findByIdAndUpdate({_id: req.params.id}, {Status : req.body.Status})
-        .then((result, err)=> {
-            if(result && !err){
-                rabbot.publish("ex.1", {
-                    routingKey: "planeNoted",
-                    type: "planeNoted",
-                    body: result
-                });
+    ChangeStatus(req, res, next) {
+        Flight.findByIdAndUpdate({ _id: req.params.id }, { Status: req.body.Status })
+            .then((result, err) => {
+                if (result && !err) {
+                    rabbot.publish("ex.1", {
+                        routingKey: "planeNoted",
+                        type: "planeNoted",
+                        body: result
+                    });
 
-                res.status(200);
-                res.send(result);
-            }else{
-                res.send(err);
-            }
-        });
+                    if (req.body.Status == 'Boarding') {
+                        rabbot.publish("ex.1", {
+                            routingKey: "stowBaggage",
+                            type: "stowBaggage",
+                            body: result
+                        });
+                    }
+
+                    res.status(200);
+                    res.send(result);
+                } else {
+                    res.send(err);
+                }
+            });
     },
 
-    PostFlight(req,res,next){
-        Plane.findById({_id: req.body.Plane}, function(err, plane){
-            Airline.findById({_id: req.body.Airline}, function(err, airline){
+    PostFlight(req, res, next) {
+        Plane.findById({ _id: req.body.Plane }, function (err, plane) {
+            Airline.findById({ _id: req.body.Airline }, function (err, airline) {
                 Flight.create({
                     FlightNumber: req.body.FlightNumber,
                     Origin: req.body.Origin,
@@ -58,22 +66,27 @@ module.exports = {
                     Gate: req.body.Gate,
                     CurrentPassengers: 0,
                     DepartDateTime: req.body.DepartDateTime
-                }, function(err, flight){
-                    if(!err){
-                        
+                }, function (err, flight) {
+                    if (!err) {
                         rabbot.publish("ex.1", {
                             routingKey: "flightNoted",
                             type: "flightNoted",
                             body: flight
                         });
-                        
+
+                        rabbot.publish("ex.1", {
+                            routingKey: "statusChanged",
+                            type: "statusChanged",
+                            body: flight.Status
+                        });
+
                         res.status(200).json({
                             status: {
                                 query: 'OK'
                             },
                             result: flight
                         });
-                    }else{
+                    } else {
                         res.send(err);
                     }
                 })
@@ -81,15 +94,15 @@ module.exports = {
         })
     },
 
-    PostPlane(req, res, next){
+    PostPlane(req, res, next) {
         Plane.create({
             Name: "Boeing 747-Max",
             CarryCapacity: 100,
             PassengerLimit: 75
-        }, function(err, plane){
-            if(!err){
+        }, function (err, plane) {
+            if (!err) {
                 res.status(200).json({
-                    status:{
+                    status: {
                         query: 'OK'
                     },
                     result: plane
@@ -98,15 +111,15 @@ module.exports = {
         })
     },
 
-    PostAirline(req, res, next){
-        Plane.findById({_id: req.body.plane}, function(err, plane){
+    PostAirline(req, res, next) {
+        Plane.findById({ _id: req.body.plane }, function (err, plane) {
             Airline.create({
                 Name: "KLM",
                 Fleet: [plane]
-            }, function(err, airline){
-                if(!err){
+            }, function (err, airline) {
+                if (!err) {
                     res.status(200).json({
-                        status:{
+                        status: {
                             query: 'OK'
                         },
                         result: airline
--- domain/flightmanagement/rabbot/rabbot.js
+++ domain/flightmanagement/rabbot/rabbot.js
@@ -7,60 +7,60 @@ const user = require('../models/users');
 require("dotenv").config();
 
 rabbot
-  .configure({
-    connection: {
-      name: "default",
-      user: process.env.RABBIT_USER,
-      pass: process.env.RABBIT_PASS,
-      host: process.env.RABBIT_HOST,
-      port: 5672,
-      vhost: "%2f",
-      replyQueue: false
-    },
-    exchanges: [{ name: "ex.1", type: "direct", autoDelete: false }],
-    queues: [
-      {
-        name: "flightmanagement_queue",
-        autoDelete: false,
-        durable: true,
-        subscribe: true
-      }
-    ],
-    bindings: [
-      {
-        exchange: "ex.1",
-        target: "flightmanagement_queue",
-        keys: ["passengerNoted", "flightNoted", "planeNoted"]
-      }
-    ]
-  })
-  .then(() => {
-    console.log("Rabbot succesfully connected.");
-    rabbot.startSubscription("flightmanagement_queue");
-    console.log("Rabbot subscribed.");
-  })
-  .catch(error => console.log("Rabbot connect error: " + error));
+    .configure({
+        connection: {
+            name: "default",
+            user: process.env.RABBIT_USER,
+            pass: process.env.RABBIT_PASS,
+            host: process.env.RABBIT_HOST,
+            port: 5672,
+            vhost: "%2f",
+            replyQueue: false
+        },
+        exchanges: [{ name: "ex.1", type: "direct", autoDelete: false }],
+        queues: [
+            {
+                name: "flightmanagement_queue",
+                autoDelete: false,
+                durable: true,
+                subscribe: true
+            }
+        ],
+        bindings: [
+            {
+                exchange: "ex.1",
+                target: "flightmanagement_queue",
+                keys: ["passengerNoted", "flightNoted", "planeNoted", "statusChanged", "stowBaggage"]
+            }
+        ]
+    })
+    .then(() => {
+        console.log("Rabbot succesfully connected.");
+        rabbot.startSubscription("flightmanagement_queue");
+        console.log("Rabbot subscribed.");
+    })
+    .catch(error => console.log("Rabbot connect error: " + error));
 
 
 rabbot.handle("flightNoted", msg => {
     new flight(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
+        .save()
+        .then(() => msg.ack())
+        .catch(err => msg.nack());
+});
 
-  rabbot.handle("passengerNoted", msg => {
+rabbot.handle("passengerNoted", msg => {
     new passenger(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
-  
-  rabbot.handle("planeNoted", msg => {
+        .save()
+        .then(() => msg.ack())
+        .catch(err => msg.nack());
+});
+
+rabbot.handle("planeNoted", msg => {
     new plane(msg)
-      .save()
-      .then(() => msg.ack())
-      .catch(err => msg.nack());
-  });
+        .save()
+        .then(() => msg.ack())
+        .catch(err => msg.nack());
+});
 
 module.exports = rabbot;
