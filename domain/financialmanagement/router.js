33e931be40409a16a00f0d3b8e4e0a76a7024977
Wouter Jansen <wouter97@planet.nl>
2019-07-01 15:08:28 +0200

    Re-added some old files

--- domain/financialmanagement/router.js
+++ domain/financialmanagement/router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 //Declare any routes here.
-const financeRouter = require("./routes/department_routes.js");
+const borderRouter = require("./routes/border_routes.js");
 
-router.use("/finance", financeRouter);
+router.use("/border", borderRouter);
 
 router.use((error, req, res, next) => {
   res.status(error.header || 500).send({
